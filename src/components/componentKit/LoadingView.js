742368c2f0dc2f5e3778c1dc1bd74ac2747ed157
ArtiomVao <vaomamago@gmail.com>
2016-12-10 18:44:14 +0300

    Add logout button in success payment page

--- src/components/componentKit/LoadingView.js
+++ src/components/componentKit/LoadingView.js
@@ -1,11 +1,26 @@
 import React from 'react'
+import cookie from 'react-cookie'
+import { browserHistory } from 'react-router'
 
-const LoadingView = ({title}) => (
+const LoadingView = ({title, logout}) => (
   <div className="layout layout--login">
     <div className="entry entry--sign-in">
       <div className="entry__inner">
         <div className="entry__box">
           <h2>{title}</h2>
+          {logout &&
+            <div>
+              <br/>
+              <button className="btn btn--action" onClick={e => {
+                e.preventDefault()
+                cookie.remove('token', { path: '/' })
+                cookie.remove('txId', { path: '/' })
+                browserHistory.push('/')
+              }}>
+                Выйти
+              </button>
+            </div>
+          }
         </div>
       </div>
     </div>
--- src/components/profile/SuccessProfile.js
+++ src/components/profile/SuccessProfile.js
@@ -2,7 +2,10 @@ import React from 'react'
 import LoadingView from '../componentKit/LoadingView'
 
 const SuccessProfile = () => (
-  <LoadingView title="Оплата прошла успешно! Вам придет подтверждение об оплате с кодом на ваш Email"/>
+  <LoadingView
+    title="Оплата прошла успешно! Вам придет подтверждение об оплате с кодом на ваш Email"
+    logout={true}
+  />
 )
 
 export default SuccessProfile
