318642a259357f3dbb674cb14046e8182c860c82
StGeass <st.geass@gmail.com>
2017-01-06 15:44:40 +0300

    [hot fix] Add link entity to Editor (client-side)

--- src/components/todayTask/TaskIntro.js
+++ src/components/todayTask/TaskIntro.js
@@ -1,12 +1,39 @@
 import React from 'react'
 import {
+  Entity,
   Editor,
   EditorState,
   convertFromRaw,
-} from 'draft-js';
+  CompositeDecorator
+} from 'draft-js'
+
+const decorator = new CompositeDecorator([
+  {
+    strategy: (contentBlock, callback) => {
+      contentBlock.findEntityRanges(
+        (character) => {
+          const entityKey = character.getEntity()
+          return (
+            entityKey !== null &&
+            Entity.get(entityKey).getType() === 'LINK'
+          )
+        },
+        callback
+      )
+    },
+    component: (props) => {
+      const {url} = Entity.get(props.entityKey).getData()
+      return (
+        <a href={url}>
+          {props.children}
+        </a>
+      )
+    },
+  },
+])
 
 const TaskIntro = ({text = '', json}) => {
-  const editorState = json ? EditorState.createWithContent(convertFromRaw(json)) : EditorState.createEmpty()
+  const editorState = json ? EditorState.createWithContent(convertFromRaw(json), decorator) : EditorState.createEmpty()
 
   return (
     <div className="stage-box stage-box--big-padding">
